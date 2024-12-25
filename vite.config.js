ae5af61d4e3e9bfdca19bbdb291a6f8b6d0015ea
aashapun11 <106722144+aashapun11@users.noreply.github.com>
2024-12-25 19:44:02 +0545

    final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://aashapun11.github.io/tictactoe",
+  base: "/tictactoe/",
 })
